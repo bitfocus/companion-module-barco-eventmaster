8a47f297d2ac378599b26b0ee09e02baea68b02e
Jeffrey Davidsz <>
2018-07-02 10:09:50 +0200

    Bugfix! Presetlist is back

--- eventmaster.js
+++ eventmaster.js
@@ -38,49 +38,50 @@ instance.prototype.updateConfig = function(config) {
 instance.prototype.retry = function() {
 	var self = this;
 
-	if (self.eventmaster === undefined || (self.config !== undefined && self.config
-			.host !== undefined && self.config.host.match(/^\d+\.\d+\.\d+\.\d+$/))) {
-		if (self.eventmaster === undefined || (self.eventmaster.ip !== undefined &&
-				self.config.host !== self.eventmaster.ip)) {
+	if (self.eventmaster === undefined || (self.config !== undefined && self.config.host !== undefined && self.config.host.match(/^\d+\.\d+\.\d+\.\d+$/)) ) {
+		if (self.eventmaster === undefined || (self.eventmaster.ip !== undefined && self.config.host !== self.eventmaster.ip)) {
 
 			var check = checkIp(self.config.host);
 			if (check.isValid === true) {
 				var cfg = {
 					timeout: 4,
 				};
-				ping.sys.probe(self.config.host, function(isAlive) {
+				ping.sys.probe(self.config.host, function(isAlive){
 					if (isAlive == true) {
 						self.eventmaster = new EventMaster(self.config.host);
 						self.status(self.STATE_OK);
-					} else {
-						self.status(self.STATE_ERROR, 'No ping reply from ' + self.config.host);
+						log('info', 'Connecting to '+self.config.host)
+					}
+					else {
+						self.status(self.STATE_ERROR, 'No ping reply from '+self.config.host);
+						log('error','No ping reply from '+self.config.host+'??')
 					}
 				}, cfg);
-			} else {
-				self.status(self.STATE_ERROR, 'Invalid IP configured');
 			}
 		}
 	}
-
 	self.actions();
 }
 
 // Return config fields for web config
-instance.prototype.config_fields = function() {
+instance.prototype.config_fields = function () {
 	var self = this;
-	return [{
-		type: 'text',
-		id: 'info',
-		width: 12,
-		label: 'Information',
-		value: 'This module uses the official EventMaster JSON API. Unfortunately the JSON API is not available in the simulator, so you need to use the real deal to get this working. If the status is OK, it ONLY means that the IP configured answers on icmp ping.'
-	}, {
-		type: 'textinput',
-		id: 'host',
-		label: 'Target IP',
-		width: 6,
-		regex: self.REGEX_IP
-	}]
+	return [
+		{
+			type: 'text',
+			id: 'info',
+			width: 12,
+			label: 'Information',
+			value: 'This module uses the official EventMaster JSON API. Unfortunately the JSON API is not available in the simulator, so you need to use the real deal to get this working.'
+		},
+		{
+			type: 'textinput',
+			id: 'host',
+			label: 'Target IP',
+			width: 6,
+			regex: self.REGEX_IP
+		}
+	]
 };
 
 // When module gets deleted
@@ -102,13 +103,8 @@ instance.prototype.actions = function(system) {
 	var self = this;
 
 	var actions = {
-		'trans_all': {
-			label: 'Take/Trans Active'
-		},
-		'cut_all': {
-			label: 'Cut Active'
-		},
-		//next step would be to load the inputdata from JSON
+		'trans_all': { label: 'Take/Trans Active' },
+		'cut_all': { label: 'Cut Active' },
 		'freeze': {
 			label: 'Freeze',
 			options: [{
@@ -141,6 +137,7 @@ instance.prototype.actions = function(system) {
 				regex: self.REGEX_NUMBER
 			}]
 		}
+
 	};
 
 	if (self.eventmaster !== undefined) {
@@ -153,25 +150,21 @@ instance.prototype.actions = function(system) {
 
 					var p_name = 'Recall preset in PVW: ' + preset.Name;
 					var pg_name = 'Recall preset in PGM: ' + preset.Name;
-					var p_id = 'recall_preset_pvw_id_' + preset.id;
-					var pg_id = 'recall_preset_pgm_id_' + preset.id;
+					var	p_id = 'recall_preset_pvw_id_' + preset.id;
+					var	pg_id = 'recall_preset_pgm_id_' + preset.id;
 
-					actions[p_id] = {
-						label: p_name
-					};
-					actions[pg_id] = {
-						label: pg_name
-					};
+					actions[p_id] = { label: p_name };
+					actions[pg_id] = { label: pg_name };
 
 				}
 			}
 
+			self.system.emit('instance_actions', self.id, actions);
+
 		}).on('error', function(err) {
-			log('error', 'EventMaster Error: ' + err);
+			log('error','EventMaster Error: '+ err);
 		});
 	}
-
-	self.system.emit('instance_actions', self.id, actions);
 }
 
 instance.prototype.action = function(action) {
@@ -184,35 +177,47 @@ instance.prototype.action = function(action) {
 		var ida = id.split(/_/);
 		var id = ida[4];
 		if (self.eventmaster !== undefined) {
-			log('info', 'Recall to PVW id:' + id)
+			log('info','Recall to PVW id:' + id)
 			self.eventmaster.activatePresetById(parseInt(id), 0, function(obj, res) {
 				debug('recall preset pvw response', res);
 			}).on('error', function(err) {
-				log('error', 'EventMaster Error: ' + err);
+				log('error','EventMaster Error: '+ err);
 			});
 		}
-	} else if (id.match(/^recall_preset_pgm_id_/)) {
+	}
+	else if (id.match(/^recall_preset_pgm_id_/)) {
 		var ida = id.split(/_/);
 		var id = ida[4];
 		if (self.eventmaster !== undefined) {
-			log('info', 'Recall to PGM id:' + id)
-
+			log('info','Recall to PGM id:' + id)
 			self.eventmaster.activatePresetById(parseInt(id), 1, function(obj, res) {
 				debug('recall preset pgm response', res);
 			}).on('error', function(err) {
-				log('error', 'EventMaster Error: ' + err);
+				log('error','EventMaster Error: '+ err);
 			});
 		}
-	} else if (id == 'trans_all') {
-		log('info', 'Trans/Take All');
+	}
+
+	else if (id == 'trans_all') {
+		log('info','Trans/Take All');
 
 		if (self.eventmaster !== undefined) {
 			self.eventmaster.allTrans(function(obj, res) {
 				debug('trans all response', res);
 			}).on('error', function(err) {
-				log('error', 'EventMaster Error: ' + err);
+				log('error','EventMaster Error: '+ err);
 			});
 
+		}
+	}	else if (id == 'cut_all') {
+		log('info','Cut All');
+
+		if (self.eventmaster !== undefined) {
+			self.eventmaster.cut(function(obj, res) {
+				debug('cut all response', res);
+			}).on('error', function(err) {
+				log('error','EventMaster Error: '+ err);
+			});
 		}
 	} else if (id == 'freeze') {
 		log('info', 'freeze');
@@ -234,24 +239,14 @@ instance.prototype.action = function(action) {
 				log('error', 'EventMaster Error: ' + err);
 			});
 		}
-	} else if (id == 'cut_all') {
-		log('info', 'Cut All');
-
-		if (self.eventmaster !== undefined) {
-			self.eventmaster.cut(function(obj, res) {
-				debug('cut all response', res);
-			}).on('error', function(err) {
-				log('error', 'EventMaster Error: ' + err);
-			});
-		}
-	}
+}
 
 };
 
 instance.module_info = {
 	label: 'Barco EventMaster JSON',
 	id: 'eventmaster',
-	version: '0.0.3'
+	version: '0.0.4'
 };
 
 instance_skel.extendedBy(instance);
